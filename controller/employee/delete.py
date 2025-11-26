29e3cb5796e4f453a04e37d1dcdef79e7b2daaa6
siyabdev <siyabwebdev@gmail.com>
2025-11-27 04:29:22 +0500

    Update

--- controller/employee/delete.py
+++ controller/employee/delete.py
@@ -13,33 +13,33 @@ delete_bp = Blueprint("delete_bp", __name__, url_prefix="/employee")
 @delete_bp.route("/delete", methods=["DELETE"])
 def delete_employee():
     data = request.json
-    username = data.get("username")
-
-    if not username:
-        app.logger.error("Username required")
-        return jsonify({"error": "Username required"}), 400
-    
-
-    employee_by_username = get_employee(username)
-
-    if not employee_by_username:
-        app.logger.info("Employee doesnt exist.")
-        return jsonify({
-            "CODE": "EMPLOYEE_DOESNT_EXIST",
-            "message": "Employee doesnt exist, please enter a valid username"
-        })
-
     try:
-        delete_query = delete_employee_crud(username=username)
+        username = data.get("username")
+
+        if not username:
+            app.logger.error("Username required")
+            return jsonify({"error": "Username required"}), 400
         
-    except IntegrityError as error:
-        app.logger.error("Integrirty error")
-        return jsonify({
-            "CODE":"INTEGRITY_ERROR_OCCURED",
-            "message":f"INTEGRITY error occured for employee '{username}' deletion, {error}"
-        })
 
-    try:
+        employee_by_username = get_employee(username)
+
+        if not employee_by_username:
+            app.logger.info("Employee doesnt exist.")
+            return jsonify({
+                "CODE": "EMPLOYEE_DOESNT_EXIST",
+                "message": "Employee doesnt exist, please enter a valid username"
+            })
+
+        try:        
+            delete_query = delete_employee_crud(username=username)
+            
+        except IntegrityError as error:
+            app.logger.error("Integrirty error")
+            return jsonify({
+                "CODE":"INTEGRITY_ERROR_OCCURED",
+                "message":f"INTEGRITY error occured for employee '{username}' deletion, {error}"
+            })
+
         if delete_query:
                 app.logger.info("Employee deleted")
                 return jsonify({
